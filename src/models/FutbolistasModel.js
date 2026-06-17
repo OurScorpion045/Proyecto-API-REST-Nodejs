221e83379c1e4e4ad3fca31122a0f465a53595a5
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-06-17 11:38:03 -0700

    Avance: Archivo FutbolistasModel.js

--- src/models/FutbolistasModel.js
+++ src/models/FutbolistasModel.js
@@ -6,11 +6,8 @@ class FutbolistasModel {
         this.connection = null;
     }
 
-    async init() {
-        this.connection = await this.database.connection();
-    }
-
     async getAllFutbolistas() {
+        this.connection = await this.database.connection();
         try {
             const sql = "SELECT * FROM `futbolistas` ORDER BY `id` DESC";
             const [rows, fields] = await this.connection.execute(sql);
@@ -19,10 +16,52 @@ class FutbolistasModel {
             console.log("Error al obtener futbolistas: " + err);
         }
     }
+
+    async getFutbolistasById(id) {
+        this.connection = await this.database.connection();
+        try {
+            const sql = "SELECT * FROM `futbolistas` WHERE `id` = ?";
+            const [rows, fields] = await this.connection.execute(sql, [id]);
+            console.log(rows);
+        } catch (err) {
+            console.log("Error al obtener futbolista: " + err);
+        }
+    }
+
+    async insertFutbolistas(nombre, posicion, numero, edad, equipo) {
+        this.connection = await this.database.connection();
+        try {
+            const sql = "INSERT INTO `futbolistas`(`nombre`, `posicion`, `numero`, `edad`, `equipo`) VALUES (?, ?)";
+            const [values] = [nombre, posicion, numero, edad, equipo];
+
+            const [result, fields] = await this.connection.execute(sql, values);
+            console.log("Futbolista insertado correctamente: " + result);
+        } catch (err) {
+            console.error("Error al insertar futbolista: " + err);
+        }
+    }
+
+    async updateFutbolistas(id, nombre, posicion, numero, edad, equipo) {
+        this.connection = await this.database.connection();
+        try {
+            const sql = "UPDATE `futbolistas` SET `nombre` = ?, `posicion` = ?, `numero` = ?, `edad` = ?, `equipo` = ? WHERE `id` = ?";
+            const values = [nombre, posicion, numero, edad, equipo, id];
+            const [results, fields] = await this.connection.execute(sql, values);
+            console.log("Futbolista actualizado correctamente: " + result);
+        } catch (err) {
+            console.error("Error al actualizar futbolista: " + err);
+        }
+    }
+
+    async deleteFutbolista(id) {
+        this.connection = await this.database.connection();
+        try {
+            const sql = "DELETE FROM `futbolistas` WHERE `id` = ?";
+            const values = [id];
+            const [results, fields] = await this.connection.execute(sql, values);
+            console.log("Futbolista eliminado correctamente: " + results);
+        } catch (err) {
+            console.error("Error al eliminar futbolista: " + err);
+        }
+    }
 }
-
-const classTest = new FutbolistasModel();
-
-await classTest.init();
-
-await classTest.getAllFutbolistas();
