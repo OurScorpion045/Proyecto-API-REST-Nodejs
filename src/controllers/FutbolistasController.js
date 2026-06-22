312c837b81e20308dca6213fb1ebe93b9ac5c62c
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-06-22 10:28:22 -0700

    Avance: Archivo FutbolistasControllers.js

---
+++ src/controllers/FutbolistasController.js
@@ -0,0 +1,11 @@
+import FutbolistasModel from "../models/FutbolistasModel.js";
+
+class FutbolistasController {
+    constructor() {
+        this.model = new FutbolistasModel();
+    }
+
+    async getAllFutbolistas() {
+        
+    }
+}
