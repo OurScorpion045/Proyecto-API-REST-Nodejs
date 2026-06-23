4e72ffa1d08a2a3d2ca8b0bb776fc18452bf0efc
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-06-22 17:58:32 -0700

    Avance: Archivo FutbolistasController.js

--- src/controllers/FutbolistasController.js
+++ src/controllers/FutbolistasController.js
@@ -1,11 +1,51 @@
 import FutbolistasModel from "../models/FutbolistasModel.js";
 
-class FutbolistasController {
-    constructor() {
-        this.model = new FutbolistasModel();
+export class FutbolistasController {
+
+    static async getAllFutbolistas(req, res) {
+        try {
+            const futbolistas = await FutbolistasModel.getAllFutbolistas();
+
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+
+            res.end(JSON.stringify(futbolistas));
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify({
+                error: err.message
+            }));
+        }
     }
 
-    async getAllFutbolistas() {
-        
+    static async getFutbolistaById(req, res, id) {
+        try {
+            const futbolista = await FutbolistasModel.getFutbolistaById(id);
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+
+            res.end(JSON.stringify(futbolista));
+        } catch(err) {
+            res.writeHead(500);
+            res.end(JSON.stringify({
+                error: err.message
+            }));
+        }
+    }
+
+    static async insertFutbolista(req, res, nombre, posicion, numero, edad, equipo) {
+        try {
+            const mensajeExito = await FutbolistasModel.insertFutbolista(nombre, posicion, numero, edad, equipo);
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            })
+        } catch(err) {
+            res.writeHead(500);
+            res.end(JSON.stringify({
+                error: err.message
+            }));
+        }
     }
 }
