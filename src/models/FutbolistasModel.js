a86ae25aa17c3669e46c486c3f12837757daf4de
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-06-22 17:59:12 -0700

    Correccion: Archivo FutbolistasModel.js

--- src/models/FutbolistasModel.js
+++ src/models/FutbolistasModel.js
@@ -1,60 +1,59 @@
 import db from "../config/Database.js";
 
-class FutbolistasModel {
-    constructor() {
-        this.database = new db();
+export class FutbolistasModel {
+    constructor () {
         this.connection = null;
     }
 
-    async getAllFutbolistas() {
-        this.connection = await this.database.connection();
+    static async getAllFutbolistas() {
+        this.connection = await db.connection();
         try {
             const sql = "SELECT * FROM `futbolistas` ORDER BY `id` DESC";
             const [rows, fields] = await this.connection.execute(sql);
-            console.log(rows);
+            return rows;
         } catch (err) {
             console.log("Error al obtener futbolistas: " + err);
         }
     }
 
-    async getFutbolistasById(id) {
-        this.connection = await this.database.connection();
+    static async getFutbolistaById(id) {
+        this.connection = await db.connection();
         try {
             const sql = "SELECT * FROM `futbolistas` WHERE `id` = ?";
             const [rows, fields] = await this.connection.execute(sql, [id]);
-            console.log(rows);
+            return rows;
         } catch (err) {
             console.log("Error al obtener futbolista: " + err);
         }
     }
 
-    async insertFutbolistas(nombre, posicion, numero, edad, equipo) {
-        this.connection = await this.database.connection();
+    static async insertFutbolista(nombre, posicion, numero, edad, equipo) {
+        this.connection = await db.connection();
         try {
             const sql = "INSERT INTO `futbolistas`(`nombre`, `posicion`, `numero`, `edad`, `equipo`) VALUES (?, ?)";
             const [values] = [nombre, posicion, numero, edad, equipo];
 
             const [result, fields] = await this.connection.execute(sql, values);
-            console.log("Futbolista insertado correctamente: " + result);
+            return "Futbolista insertado correctamente: " + result;
         } catch (err) {
             console.error("Error al insertar futbolista: " + err);
         }
     }
 
-    async updateFutbolistas(id, nombre, posicion, numero, edad, equipo) {
-        this.connection = await this.database.connection();
+    static async updateFutbolista(id, nombre, posicion, numero, edad, equipo) {
+        this.connection = await db.connection();
         try {
             const sql = "UPDATE `futbolistas` SET `nombre` = ?, `posicion` = ?, `numero` = ?, `edad` = ?, `equipo` = ? WHERE `id` = ?";
             const values = [nombre, posicion, numero, edad, equipo, id];
             const [results, fields] = await this.connection.execute(sql, values);
-            console.log("Futbolista actualizado correctamente: " + result);
+            return "Futbolista actualizado correctamente: " + result;
         } catch (err) {
             console.error("Error al actualizar futbolista: " + err);
         }
     }
 
-    async deleteFutbolista(id) {
-        this.connection = await this.database.connection();
+    static async deleteFutbolista(id) {
+        this.connection = await db.connection();
         try {
             const sql = "DELETE FROM `futbolistas` WHERE `id` = ?";
             const values = [id];
