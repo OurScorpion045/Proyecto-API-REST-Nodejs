404485c619874c50078c2fe3e2f97fd609a3e609
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-06-23 10:13:11 -0700

    Nuevo archivo: parseBody.js

---
+++ src/utils/parseBody.js
@@ -0,0 +1,15 @@
+export const parseBody = (req) => {
+    return new Promise((resolve, reject) => {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            resolve(JSON.parse(body));
+        });
+
+        req.on('error', reject);
+    });
+};
