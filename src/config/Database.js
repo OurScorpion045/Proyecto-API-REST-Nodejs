b9c3733567152f7884af7c006bf5edb0b633e90b
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-06-24 13:17:24 -0700

    correccion: Archivo Database.js

--- src/config/Database.js
+++ src/config/Database.js
@@ -3,20 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export default class Database {
-    constructor() {
-        this.host = process.env.MYSQL_HOST;
-        this.user = process.env.MYSQL_USER;
-        this.password = process.env.MYSQL_PASSWORD;
-        this.database = process.env.MYSQL_DATABASE;
-    }
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
 
-    static async connection() {
-        return await mysql.createPool({
-            host: this.host,
-            user: this.user,
-            password: this.password,
-            database: this.database
-        });
-    }
-}
+export default pool;
